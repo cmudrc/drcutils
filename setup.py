0f948a73ee32b73843f017e98608757ea2053f1e
Chris McComb <ccmcc2012@gmail.com>
2022-12-10 17:20:50 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='drcutils',
+      version='0.1.0',
+      description='DRC Utilities',
+      author='Chris McComb',
+      author_email='ccmcc2012@gmail.com',
+      url='https://github.com/cmudrc/drcutils/',
+      packages=['drcutils'],
+     )
