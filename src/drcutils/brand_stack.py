694286c87685cdede345a95a5479cc7c03f8dfbf
Chris McComb <ccmcc2012@gmail.com>
2022-12-11 15:20:41 -0500

    Update brand_stack.py

--- src/drcutils/brand_stack.py
+++ src/drcutils/brand_stack.py
@@ -13,15 +13,15 @@ class logo_only:
     IMAGE_OBJECT = PIL.Image.open(PNG_PATH)
     
     
-# class horizontal_logo:
-#     import pkg_resources
-#     SVG = pkg_resources.resource_filename('drcutils', 'data/logo.svg')
-#     STL = pkg_resources.resource_filename('drcutils', 'data/logo.stl')
+class horizontal_logo:
+    import pkg_resources
+    PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/horizontal.png')
+    IMAGE_OBJECT = PIL.Image.open(PNG_PATH)
 
-# class stacked_logo:
-#     import pkg_resources
-#     SVG = pkg_resources.resource_filename('drcutils', 'data/logo.svg')
-#     STL = pkg_resources.resource_filename('drcutils', 'data/logo.stl')
+class stacked_logo:
+    import pkg_resources
+    PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/stacked.png')
+    IMAGE_OBJECT = PIL.Image.open(PNG_PATH)
 
 COLORS = [
     "#000000",
