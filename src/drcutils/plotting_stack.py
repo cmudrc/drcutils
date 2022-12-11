bcbe99721265208452ba2755ce4cbb97ff838498
Chris McComb <ccmcc2012@gmail.com>
2022-12-11 16:18:31 -0500

    Update plotting_stack.py

--- src/drcutils/plotting_stack.py
+++ src/drcutils/plotting_stack.py
@@ -1,5 +1,41 @@
 import matplotlib.pyplot
 import matplotlib.colors
+import stl
+import PIL
+import svgpathtools
+
+
+class logo_only:
+    import pkg_resources
+    SVG_PATH = pkg_resources.resource_filename('drcutils', 'data/logo.svg')
+    PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/logo.png')
+    STL_PATH = pkg_resources.resource_filename('drcutils', 'data/logo.stl')
+    SVG_OBJECT, _ = svgpathtools.svg2paths(SVG_PATH)
+    MESH_OBJECT = stl.mesh.Mesh.from_file(STL_PATH)
+    IMAGE_OBJECT = PIL.Image.open(PNG_PATH)
+    
+    
+class horizontal_logo:
+    import pkg_resources
+    PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/horizontal.png')
+    IMAGE_OBJECT = PIL.Image.open(PNG_PATH)
+
+    
+class stacked_logo:
+    import pkg_resources
+    PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/stacked.png')
+    IMAGE_OBJECT = PIL.Image.open(PNG_PATH)
+
+    
+COLORS = [
+    "#000000",
+    "#1A4C49",
+    "#4C8687",
+    "#58B7BB",
+    "#EA8534",
+    "#DF5227"
+]
+
 
 
 diverging_hamster_colormap = matplotlib.colors.LinearSegmentedColormap(
