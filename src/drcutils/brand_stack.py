19117ccab4dee80d6ca9307d3ff29f60457c882b
Chris McComb <ccmcc2012@gmail.com>
2022-12-10 21:33:20 -0500

    Update and rename logo.py to brand_stack.py

--- src/drcutils/brand_stack.py
+++ src/drcutils/brand_stack.py
@@ -2,3 +2,12 @@ class only:
     import pkg_resources
     SVG = pkg_resources.resource_filename('drcutils', 'data/logo.svg')
     STL = pkg_resources.resource_filename('drcutils', 'data/logo.stl')
+
+COLORS = [
+    "#000000",
+    "#1A4C9",
+    "#4C8687",
+    "#58B7BB",
+    "#EA8534",
+    "#DF5227"
+]
