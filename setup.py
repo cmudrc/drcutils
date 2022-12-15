64651d8fdf6c4d1542f1709ecc44bfa65baaddcb
Chris McComb <ccmcc2012@gmail.com>
2022-12-15 18:44:38 -0500

    Reflecting lab ownership and authorship

--- setup.py
+++ setup.py
@@ -4,9 +4,9 @@ from setuptools import setup, find_packages
 
 setup(name='drcutils',
       version='0.1.0',
-      description='DRC Utilities',
-      author='Chris McComb',
-      author_email='ccmcc2012@gmail.com',
+      description='Utilities for research in the Design Research Collective',
+      author='The Design Research Collective',
+      author_email='ask-drc@andrew.cmu.edu',
       url='https://github.com/cmudrc/drcutils/',      
       install_requires=["numpy", "matplotlib", "huggingface_hub", "datasets", "gradio", "numpy-stl", "Pillow", "svgpathtools", "plotly"],
       packages=['drcutils'],
