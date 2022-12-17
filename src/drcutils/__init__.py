8e639655027d80ed435140d2fc0d5139c2abe1f1
Chris McComb <ccmcc2012@gmail.com>
2022-12-17 13:41:02 -0500

    Update __init__.py

--- src/drcutils/__init__.py
+++ src/drcutils/__init__.py
@@ -1,3 +1,19 @@
-def is_colab():
+def is_colab() -> bool:
     import sys
     return 'google.colab' in sys.modules
+
+def is_notebook() -> bool:
+    if is_colab():
+        return True
+    else:
+        try:
+            shell = get_ipython().__class__.__name__
+            print(shell)
+            if shell == 'ZMQInteractiveShell':
+                return True   # Jupyter notebook or qtconsole
+            elif shell == 'TerminalInteractiveShell':
+                return False  # Terminal running IPython
+            else:
+                return False  # Other type (?)
+        except NameError:
+            return False      # Probably standard Python interpreter
