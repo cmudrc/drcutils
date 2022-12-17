ee645414b2ccb206fe0e8c182e5c549d07a9764d
Chris McComb <ccmcc2012@gmail.com>
2022-12-17 13:44:16 -0500

    Update hack_stack.py

--- src/drcutils/hack_stack.py
+++ src/drcutils/hack_stack.py
@@ -5,3 +5,23 @@ import keras
 import matplotlib.pyplot
 import sklearn
 import gradio
+
+def visualize_network(path: str, height: int = 500, port: int = 8000):
+    from netron import serve
+    from IPython.display import Javascript
+    # url = eval_js("google.colab.kernel.proxyPort(8000)")
+    if is_notebook():
+        serve("mobilenet.h5", None, ("localhost", port), False, 0)
+        display(Javascript("""
+        (async ()=>{
+            fm = document.createElement('iframe')
+            fm.src = await google.colab.kernel.proxyPort(%s)
+            fm.width = '95%%'
+            fm.height = '%d'
+            fm.frameBorder = 0
+            document.body.append(fm)
+        })();
+        """ % (port, height) ))
+    else:
+        serve("mobilenet.h5", None, ("localhost", port), True, 0)
+    
