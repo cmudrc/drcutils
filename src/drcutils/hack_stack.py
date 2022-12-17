3bd1a817a440dac04643e6eaed4f1f6e7bbcc2ff
Chris McComb <ccmcc2012@gmail.com>
2022-12-17 13:49:09 -0500

    Update hack_stack.py

--- src/drcutils/hack_stack.py
+++ src/drcutils/hack_stack.py
@@ -5,12 +5,12 @@ import keras
 import matplotlib.pyplot
 import sklearn
 import gradio
+import drcutils
 
 def visualize_network(path: str, height: int = 500, port: int = 8000):
     from netron import serve
     from IPython.display import Javascript
-    # url = eval_js("google.colab.kernel.proxyPort(8000)")
-    if is_notebook():
+    if drcutils.is_notebook():
         serve("mobilenet.h5", None, ("localhost", port), False, 0)
         display(Javascript("""
         (async ()=>{
