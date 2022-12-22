f1a6d135edf2602ac07c4ca29b8dced31f4db362
Chris McComb <ccmcc2012@gmail.com>
2022-12-22 12:07:38 -0500

    Added reversed colormaps

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,11 @@
+from __future__ import annotations
+
+
 def is_colab() -> bool:
     """Returns true if the function is being run in a Google Colab notebook and false if not."""
     import sys
     return 'google.colab' in sys.modules
+
 
 def is_notebook() -> bool:
     """Returns true if the function is being run in a notebook of any sort and false if not."""
@@ -19,3 +23,35 @@ def is_notebook() -> bool:
                 return False  # Other type (?)
         except NameError:
             return False      # Probably standard Python interpreter
+
+
+def gimme(numpy: bool | str = "np",
+          pandas: bool | str = "pd",
+          matplotlib_pyplot: bool | str = "plt",
+          matplotlib_colors: bool | str = "mpc",
+          pillow: bool | str = "PIL",
+          datasets: bool | str = "data",
+          huggingface_hub: bool | str = "hub",
+          gradio: bool | str = "gr",
+          plotly_express: bool | str = "px",
+          plotly_graph_objects: bool | str = "go",
+          **kwargs):
+    """Imports libraries programmatically"""
+
+    import importlib
+    modules = {
+        "numpy": numpy,
+        "pandas": pandas,
+        "Pillow": pillow,
+        "matplotlib.pyplot": matplotlib_pyplot,
+        "matplotlib.colors": matplotlib_colors,
+        "datasets": datasets,
+        "huggingface_hub": huggingface_hub,
+        "gradio": gradio,
+        "plotly_express": plotly_express,
+        "plotly_graph_objects": plotly_graph_objects,
+    }
+    modules.update(kwargs)
+    for module_name in modules.keys():
+        if modules[module_name]:
+            globals()[modules[module_name]] = importlib.import_module(module_name)
