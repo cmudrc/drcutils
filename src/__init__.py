721a4962d552c3d2179b73cbda471d5fd2af07d4
Chris McComb <ccmcc2012@gmail.com>
2022-12-22 14:46:22 -0500

    Put is_notebook in a better place

--- src/__init__.py
+++ src/__init__.py
@@ -1,29 +1,6 @@
 from __future__ import annotations
 
 
-def is_colab() -> bool:
-    """Returns true if the function is being run in a Google Colab notebook and false if not."""
-    import sys
-    return 'google.colab' in sys.modules
-
-
-def is_notebook() -> bool:
-    """Returns true if the function is being run in a notebook of any sort and false if not."""
-    if is_colab():
-        return True
-    else:
-        try:
-            shell = get_ipython().__class__.__name__
-            print(shell)
-            if shell == 'ZMQInteractiveShell':
-                return True   # Jupyter notebook or qtconsole
-            elif shell == 'TerminalInteractiveShell':
-                return False  # Terminal running IPython
-            else:
-                return False  # Other type (?)
-        except NameError:
-            return False      # Probably standard Python interpreter
-
 
 def gimme(numpy: bool | str = "np",
           pandas: bool | str = "pd",
