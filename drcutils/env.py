be8ff69af12c782864022964f1c868d094450653
Chris McComb <ccmcc2012@gmail.com>
2022-12-23 10:57:15 -0500

    Better imports

--- drcutils/env.py
+++ drcutils/env.py
@@ -1,17 +1,19 @@
+from IPython import get_ipython as _get_ipython
+from sys import modules as _modules
+
+
 def is_google_colab() -> bool:
     """Returns true if the function is being run in a Google Colab notebook and false if not."""
-    import sys
-    return 'google.colab' in sys.modules
+    return 'google.colab' in _modules
 
 
 def is_notebook() -> bool:
     """Returns true if the function is being run in a notebook of any sort and false if not."""
-    from IPython import get_ipython
     if is_google_colab():
         return True
     else:
         try:
-            shell = get_ipython().__class__.__name__
+            shell = _get_ipython().__class__.__name__
             if shell == 'ZMQInteractiveShell':
                 return True   # Jupyter notebook or qtconsole
             elif shell == 'TerminalInteractiveShell':
