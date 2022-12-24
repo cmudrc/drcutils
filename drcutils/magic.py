72811c0c429a9814b3f2c8505fc5580ebf0c53f1
Chris McComb <ccmcc2012@gmail.com>
2022-12-24 15:19:32 -0500

    Hoping magic works now

--- drcutils/magic.py
+++ drcutils/magic.py
@@ -15,58 +15,61 @@ from os.path import splitext as _splitext
 #   - https://pytorch.org/docs/stable/onnx.html
 
 
-_from_data_extensions = {
-    # ".onnx": _convert,
-    # ".pt": _load,
-    # ".pth": _load,
-    ".png": _open,
-    ".jpg": _open,
-    ".jpeg": _open,
-    ".eps": _open,
-    ".bmp": _open,
-    ".csv": _pandas.read_csv,
-    ".hdf5": _pandas.read_hdf,
-    ".h5": _pandas.read_hdf,
-    ".json": _pandas.read_json,
-    ".xml": _pandas.read_xml,
-    ".parquet": _pandas.read_parquet,
-    ".xls": _pandas.read_excel,
-    ".xlsx": _pandas.read_excel,
-    ".feather": _pandas.read_feather,
-    ".dta": _pandas.read_stata,
-    ".xpt": _pandas.read_sas,
-    ".sas7bdat": _pandas.read_sas,
-    ".sav": _pandas.read_spss,
-    ".zsav": _pandas.read_spss,
-    ".pkl": _pandas.read_pickle,
-}
+def _get_data_extensions():
+    _from_data_extensions = {
+        # ".onnx": _convert,
+        # ".pt": _load,
+        # ".pth": _load,
+        ".png": _open,
+        ".jpg": _open,
+        ".jpeg": _open,
+        ".eps": _open,
+        ".bmp": _open,
+        ".csv": _pandas.read_csv,
+        ".hdf5": _pandas.read_hdf,
+        ".h5": _pandas.read_hdf,
+        ".json": _pandas.read_json,
+        ".xml": _pandas.read_xml,
+        ".parquet": _pandas.read_parquet,
+        ".xls": _pandas.read_excel,
+        ".xlsx": _pandas.read_excel,
+        ".feather": _pandas.read_feather,
+        ".dta": _pandas.read_stata,
+        ".xpt": _pandas.read_sas,
+        ".sas7bdat": _pandas.read_sas,
+        ".sav": _pandas.read_spss,
+        ".zsav": _pandas.read_spss,
+        ".pkl": _pandas.read_pickle,
+    }
 
+    _to_data_extensions = {
+        # ".onnx": NotImplemented,
+        # ".pt": _save,
+        # ".pth": _save,
+        ".png": _Image.save,
+        ".jpg": _Image.save,
+        ".jpeg": _Image.save,
+        ".eps": _Image.save,
+        ".bmp": _Image.save,
+        ".csv": _pandas.DataFrame.to_csv,
+        ".hdf5": _pandas.DataFrame.to_hdf,
+        ".h5": _pandas.DataFrame.to_hdf,
+        ".json": _pandas.DataFrame.to_json,
+        ".xml": _pandas.DataFrame.to_xml,
+        ".parquet": _pandas.DataFrame.to_parquet,
+        ".xls": _pandas.DataFrame.to_excel,
+        ".xlsx": _pandas.DataFrame.to_excel,
+        ".feather": _pandas.DataFrame.to_feather,
+        ".dta": _pandas.DataFrame.to_stata,
+        ".pkl": _pandas.DataFrame.to_pickle,
+    }
 
-_to_data_extensions = {
-    # ".onnx": NotImplemented,
-    # ".pt": _save,
-    # ".pth": _save,
-    ".png": _Image.save,
-    ".jpg": _Image.save,
-    ".jpeg": _Image.save,
-    ".eps": _Image.save,
-    ".bmp": _Image.save,
-    ".csv": _pandas.DataFrame.to_csv,
-    ".hdf5": _pandas.DataFrame.to_hdf,
-    ".h5": _pandas.DataFrame.to_hdf,
-    ".json": _pandas.DataFrame.to_json,
-    ".xml": _pandas.DataFrame.to_xml,
-    ".parquet": _pandas.DataFrame.to_parquet,
-    ".xls": _pandas.DataFrame.to_excel,
-    ".xlsx": _pandas.DataFrame.to_excel,
-    ".feather": _pandas.DataFrame.to_feather,
-    ".dta": _pandas.DataFrame.to_stata,
-    ".pkl": _pandas.DataFrame.to_pickle,
-}
+    return _to_data_extensions, _from_data_extensions
 
 
 def _convertible(from_ext: str, to_ext: str) -> bool:
     """Check if a file conversion is possible."""
+    _to_data_extensions, _from_data_extensions = _get_data_extensions()
     to_fun = str(_to_data_extensions[to_ext])
     from_fun = str(_from_data_extensions[from_ext])
     if "to_" in to_fun and "read_" in from_fun:
@@ -79,8 +82,9 @@ def _convertible(from_ext: str, to_ext: str) -> bool:
         return False
 
 
-def convert(thing_to_convert_from: str, thing_to_convert_to: str, from_kwargs: dict = {}, to_kwargs: dict = {}):
+def convert(thing_to_convert_from: str, thing_to_convert_to: str, from_kwargs: dict = dict(), to_kwargs: dict = {}):
     """Convert stuff to other stuff. Works for images and datafiles."""
+    _to_data_extensions, _from_data_extensions = _get_data_extensions()
     from_ext = _splitext(thing_to_convert_from)[1]
     to_ext = _splitext(thing_to_convert_to)[1]
 
