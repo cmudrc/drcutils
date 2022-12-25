1e6b059bb6b6e50ecd0115055ab403b4bc6e2167
Chris McComb <ccmcc2012@gmail.com>
2022-12-25 15:50:02 -0500

    Colormap helper function check

--- drcutils/colormaps.py
+++ drcutils/colormaps.py
@@ -13,86 +13,43 @@ def _make_segment_data(colors, fractions):
     }
 
 
+#: A colormap that simply contains the brand colors
+hamster = _mpc.ListedColormap(COLORS, name="hamster")
+
+#: Reversed version of `hamster`
+hamster_r = hamster.reversed()
+hamster_r.name = "hamster_r"
+
 #: Diverging colormap with white center
-diverging_hamster_colormap = _mpc.LinearSegmentedColormap(
+diverging_hamster = _mpc.LinearSegmentedColormap(
     "diverging_hamster",
     segmentdata=_make_segment_data(
         [GREEN, TEAL, TURQUOISE, WHITE, ORANGE, RED],
         [0.00, 0.17, 0.33, 0.50, 0.75, 1.00]
     ),
-    N = 256
+    N=256
 )
-#     {
-#         "red": [
-#             [0.00, GREEN[0], GREEN[0]],
-#             [0.00, TEAL[0], TEAL[0]],
-#             [0.00, TURQUOISE[0], TURQUOISE[0]],
-#             [0.50, 1.00, 1.00],
-#             [0.75, ORANGE[0], ],
-#             [1.00, 0.87, 0.87],
-#         ],
-#         "green": [
-#             [0.00, GREEN[1], GREEN[1]],
-#             [0.17, 0.53, 0.53],
-#             [0.33, 0.72, 0.72],
-#             [0.50, 1.00, 1.00],
-#             [0.75, 0.52, 0.52],
-#             [1.00, 0.32, 0.32],
-#         ],
-#         "blue": [
-#             [0.00, GREEN[2], GREEN[2]],
-#             [0.17, 0.53, 0.53],
-#             [0.33, 0.73, 0.73],
-#             [0.50, 1.00, 1.00],
-#             [0.75, 0.20, 0.20],
-#             [1.00, 0.15, 0.15],
-#         ],
-#     },
-#     N=256,
-# )
 
-#: Reversed version of `diverging_hamster_colormap`
-diverging_hamster_colormap_r = diverging_hamster_colormap.reversed()
-diverging_hamster_colormap_r.name = "diverging_hamster_r"
+#: Reversed version of `diverging_hamster`
+diverging_hamster_r = diverging_hamster.reversed()
+diverging_hamster_r.name = "diverging_hamster_r"
 
 #: Diverging colormap, black center
-dark_diverging_hamster_colormap = _mpc.LinearSegmentedColormap(
+dark_diverging_hamster = _mpc.LinearSegmentedColormap(
     "dark_diverging_hamster",
-    segmentdata={
-        "red": [
-            [0.00, 0.35, 0.35],
-            [0.17, 0.30, 0.30],
-            [0.33, 0.10, 0.10],
-            [0.50, 0.00, 0.00],
-            [0.75, 0.87, 0.87],
-            [1.00, 0.92, 0.92],
-        ],
-        "green": [
-            [0.00, 0.72, 0.72],
-            [0.17, 0.53, 0.53],
-            [0.33, 0.30, 0.30],
-            [0.50, 0.00, 0.00],
-            [0.75, 0.32, 0.32],
-            [1.00, 0.52, 0.52],
-        ],
-        "blue": [
-            [0.00, 0.73, 0.73],
-            [0.17, 0.53, 0.53],
-            [0.33, 0.29, 0.29],
-            [0.50, 0.00, 0.00],
-            [0.75, 0.15, 0.15],
-            [1.00, 0.20, 0.20],
-        ],
-    },
+    segmentdata=_make_segment_data(
+        [TURQUOISE, TEAL, GREEN, BLACK, RED, ORANGE],
+        [0.00, 0.17, 0.33, 0.50, 0.75, 1.00]
+    ),
     N=256,
 )
 
-#: Reversed version of `dark_diverging_hamster_colormap`
-dark_diverging_hamster_colormap_r = dark_diverging_hamster_colormap.reversed()
-dark_diverging_hamster_colormap_r.name = "dark_diverging_hamster_r"
+#: Reversed version of `dark_diverging_hamster`
+dark_diverging_hamster_r = dark_diverging_hamster.reversed()
+dark_diverging_hamster_r.name = "dark_diverging_hamster_r"
 
 #: A cool colormap that uses our brand colors
-cool_hamster_colormap = _mpc.LinearSegmentedColormap(
+cool_hamster = _mpc.LinearSegmentedColormap(
     "cool_hamster",
     segmentdata={
         "red": [
@@ -120,12 +77,12 @@ cool_hamster_colormap = _mpc.LinearSegmentedColormap(
     N=256,
 )
 
-#: Reversed version of `cool_hamster_colormap`
-cool_hamster_colormap_r = cool_hamster_colormap.reversed()
-cool_hamster_colormap_r.name = "cool_hamster_r"
+#: Reversed version of `cool_hamster`
+cool_hamster_r = cool_hamster.reversed()
+cool_hamster_r.name = "cool_hamster_r"
 
 #: A warm colormap that uses our brand colors
-warm_hamster_colormap = _mpc.LinearSegmentedColormap(
+warm_hamster = _mpc.LinearSegmentedColormap(
     "warm_hamster",
     segmentdata={
         "red": [
@@ -150,6 +107,6 @@ warm_hamster_colormap = _mpc.LinearSegmentedColormap(
     N=256,
 )
 
-#: Reversed version of `warm_hamster_colormap`
-warm_hamster_colormap_r = warm_hamster_colormap.reversed()
-warm_hamster_colormap_r.name = "warm_hamster_r"
+#: Reversed version of `warm_hamster`
+warm_hamster_r = warm_hamster.reversed()
+warm_hamster_r.name = "warm_hamster_r"
