4c722162676c1248233103cabb9f703a2bb83044
Chris McComb <ccmcc2012@gmail.com>
2022-12-25 15:59:41 -0500

    Added patterns to brand

--- drcutils/brand.py
+++ drcutils/brand.py
@@ -11,16 +11,25 @@ COLORS = [
 ]
 
 #: Path to an SVG of logo
-LOGO_ONLY_SVG_PATH = pkg_resources.resource_filename('drcutils', 'data/logo.svg')
+LOGO_ONLY_SVG = pkg_resources.resource_filename('drcutils', 'data/logo.svg')
 
 #: Path to a PNG of logo
-LOGO_ONLY_PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/logo.png')
+LOGO_ONLY_PNG = pkg_resources.resource_filename('drcutils', 'data/logo.png')
 
 #: Path to an STL of logo
-LOGO_ONLY_STL_PATH = pkg_resources.resource_filename('drcutils', 'data/logo.stl')
+LOGO_ONLY_STL = pkg_resources.resource_filename('drcutils', 'data/logo.stl')
 
 #: Path to a PNG of the horizontal logo
-HORIZONTAL_LOGO_PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/horizontal.png')
+HORIZONTAL_LOGO_PNG = pkg_resources.resource_filename('drcutils', 'data/horizontal.png')
 
 #: Path to a PNG of the stacked logo
-STACKED_LOGO_PNG_PATH = pkg_resources.resource_filename('drcutils', 'data/stacked.png')
+STACKED_LOGO_PNG = pkg_resources.resource_filename('drcutils', 'data/stacked.png')
+
+#: Path to a grey PNG of the patterned logo
+GREY_PATTERN_PNG = pkg_resources.resource_filename('drcutils', 'data/grey_pattern.png')
+
+#: Path to a white PNG of the patterned logo
+WHITE_PATTERN_PNG = pkg_resources.resource_filename('drcutils', 'data/white_pattern.png')
+
+#: Path to a full-color PNG of the patterned logo
+COLOR_PATTERN_PNG = pkg_resources.resource_filename('drcutils', 'data/color_pattern.png')
