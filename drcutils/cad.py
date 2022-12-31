a821ab53f9596bb96c7ad894a3d8665e09fbc50a
Chris McComb <ccmcc2012@gmail.com>
2022-12-31 15:25:21 -0500

    Import and typehint cleanup

--- drcutils/cad.py
+++ drcutils/cad.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
-from plotly.graph_objects import Mesh3d as _Mesh3d, Layout as _Layout, Figure as _Figure
-from numpy import take as _take, unique as _unique
-from stl.mesh import Mesh as _Mesh
-from os import PathLike
+import os
+
+import numpy as _numpy
+import plotly.graph_objects
+import plotly.graph_objects as _plotly_graph_objects
+import stl as _numpy_stl
 
 
-def visualize_stl(filepath: str | bytes | PathLike, color: str = "#ffffff") -> _Figure:
+def visualize_stl(filepath: str | bytes | os.PathLike, color: str = "#ffffff") -> plotly.graph_objects.Figure:
     """Visualize an STL.
 
     Displays the input STL in a Plotly Mesh3d format.
@@ -24,27 +26,31 @@ def visualize_stl(filepath: str | bytes | PathLike, color: str = "#ffffff") -> _
         A plotly figure that displays the STl file.
 
     """
-    stl_mesh = _Mesh.from_file(filepath)
+    stl_mesh = _numpy_stl.mesh.Mesh.from_file(filepath)
     # stl_mesh is read by nympy-stl from a stl file; it is  an array of faces/triangles (i.e. three 3d points)
     # this function extracts the unique vertices and the lists I, J, K to define a Plotly mesh3d
     p, q, r = stl_mesh.vectors.shape  # (p, 3, 3)
     # the array stl_mesh.vectors.reshape(p*q, r) can contain multiple copies of the same vertex;
     # extract unique vertices from all mesh triangles
-    vertices, ixr = _unique(stl_mesh.vectors.reshape(p * q, r), return_inverse=True, axis=0)
-    I = _take(ixr, [3 * k for k in range(p)])
-    J = _take(ixr, [3 * k + 1 for k in range(p)])
-    K = _take(ixr, [3 * k + 2 for k in range(p)])
+    vertices, ixr = _numpy.unique(stl_mesh.vectors.reshape(p * q, r), return_inverse=True, axis=0)
+    I = _numpy.take(ixr, [3 * k for k in range(p)])
+    J = _numpy.take(ixr, [3 * k + 1 for k in range(p)])
+    K = _numpy.take(ixr, [3 * k + 2 for k in range(p)])
     x, y, z = vertices.T
     colorscale = [[0, color], [1, color]]
 
-    mesh3D = _Mesh3d(x=x, y=y, z=z, i=I, j=J, k=K, flatshading=True, colorscale=colorscale, intensity=z, showscale=False)
-    layout = _Layout(
+    mesh3D = _plotly_graph_objects.Mesh3d(x=x, y=y, z=z, i=I, j=J, k=K, flatshading=True, colorscale=colorscale,
+                                          intensity=z, showscale=False)
+    layout = _plotly_graph_objects.Layout(
         scene_xaxis_visible=False,
         scene_yaxis_visible=False,
         scene_zaxis_visible=False,
         scene_aspectmode="data"
     )
-    fig = _Figure(data=[mesh3D], layout=layout)
+    fig = _plotly_graph_objects.Figure(data=[mesh3D], layout=layout)
     fig.data[0].update(
         lighting=dict(ambient=0.18, diffuse=1, fresnel=.1, specular=1, roughness=.1, facenormalsepsilon=0))
     return fig
+
+
+del plotly.graph_objects
